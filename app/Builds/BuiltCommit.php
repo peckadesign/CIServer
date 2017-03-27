a09bc22947a8cce98800a374c76d33d42149cc38
Milan Pála <milan.pala@peckadesign.cz>
2017-03-27 10:41:23 +0200

    Probíhá kontrola povinnosti políček

--- app/Builds/BuiltCommit.php
+++ app/Builds/BuiltCommit.php
@@ -63,6 +63,10 @@ class BuiltCommit implements \JsonSerializable
 	{
 		$json = \Nette\Utils\Json::decode($jsonString, \Nette\Utils\Json::FORCE_ARRAY);
 
+		if (empty($json['repositoryId']) || empty($json['commit'])) {
+			throw new \Nette\Utils\JsonException('Zpráva neobsahuje povinná pole');
+		}
+
 		return new self((int) $json['repositoryId'], isset($json['buildId']) ? (int) $json['buildId'] : NULL, (string) $json['commit']);
 	}
 }
