389cdd680acaa1053ff5282e1d860edd54eb2822
Milan Pála <milan.pala@peckadesign.cz>
2017-08-21 08:24:22 +0200

    Přidána podpora pro smajlíky

---
+++ migrations/structures/2017-08-21-82200-mb4.sql
@@ -0,0 +1,17 @@
+ALTER TABLE `build_requests`
+  COLLATE 'utf8mb4_czech_ci';
+
+ALTER TABLE `create_test_servers`
+  COLLATE 'utf8mb4_czech_ci';
+
+ALTER TABLE `pull_requests`
+  COLLATE 'utf8mb4_czech_ci';
+
+ALTER TABLE `pull_requests_hooks`
+  COLLATE 'utf8mb4_czech_ci';
+
+ALTER TABLE `repositories`
+  COLLATE 'utf8mb4_czech_ci';
+
+ALTER TABLE `users`
+  COLLATE 'utf8mb4_czech_ci';
